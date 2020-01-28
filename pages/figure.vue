e2e65bb49fd26307c103778f63e84844c19b0b22
shoko.miyagawa <s.mygw@hitohineri.jp>
2020-01-28 13:35:40 +0900

    Delete nav in order to mix dropdown list

--- pages/figure.vue
+++ pages/figure.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="header">
       <img src="../assets/img/logo.png" alt="ロゴ" class="header-logo" />
-      <div class="nav">
+      <!-- <div class="nav">
         <div class="nav-item">
           <ul>
             <li
@@ -17,7 +17,8 @@
             </li>
           </ul>
         </div>
-      </div>
+      </div> -->
+      <div class="header-test">aaaa</div>
     </div>
     <div class="container">
       <div class="figure-field">
@@ -26,7 +27,6 @@
     </div>
   </div>
 </template>
-
 <script lang="ts">
 import { Component, Vue, Prop } from 'nuxt-property-decorator'
 import Logo from '~/components/atoms/Logo.vue'
@@ -50,85 +50,80 @@ export default class extends Vue {
   }
 }
 </script>
-
 <style scoped lang="scss">
 a {
   text-decoration: none;
 }
-
 .container {
   display: grid;
-
   height: 100vh;
   margin: 0 auto;
   text-align: center;
-
   grid-template-columns: repeat(12, 1fr);
   grid-template-rows: repeat(12, 1fr);
 }
-
 .header {
   position: relative;
   grid-row: 4 / 8;
   grid-column: 4 / 8;
   z-index: 10;
   width: 100%;
-  height: 20vh;
-  min-height: 120px;
-  max-height: 180px;
-  text-align: center;
+  height: 8vh;
+  min-height: 50px;
+  max-height: 60px;
+  text-align: left;
   background: #f5c9c6;
   box-shadow: 0 0 10px 0;
-
   &-logo {
-    width: 25%;
-    height: 15vh;
-    min-height: 87px;
-    max-height: 180px;
+    width: 10%;
+    height: 6vh;
+    min-height: 50px;
+    max-height: 80px;
+    margin: 0 0 0 auto;
+  }
+  &-test {
+    float: right;
+    height: 6vh;
+    min-height: 50px;
+    max-height: 80px;
+    background: #6f1f1f;
   }
 }
-
-.nav {
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-  height: 5vh;
-  min-height: 30px;
-  max-height: 50px;
-  color: #000;
-  text-align: center;
-  background: linear-gradient(#f5c9c6, #feeeed);
-  border: #feeeed;
-  border-style: solid;
-
-  &-logo {
-    display: inline-block;
-  }
-
-  &-item {
-    display: inline-block;
-
-    &-content {
-      margin: 2px 20px 2px 0;
-      cursor: pointer;
-
-      // background: #fff;
-    }
-  }
-}
+// .nav {
+//   position: absolute;
+//   bottom: 0;
+//   width: 100%;
+//   height: 5vh;
+//   min-height: 30px;
+//   max-height: 50px;
+//   color: #000;
+//   text-align: center;
+//   background: linear-gradient(#f5c9c6, #feeeed);
+//   border: #feeeed;
+//   border-style: solid;
+//   &-logo {
+//     display: inline-block;
+//   }
+//   &-item {
+//     display: inline-block;
+//     &-content {
+//       margin: 2px 20px 2px 0;
+//       cursor: pointer;
+//       // background: #fff;
+//     }
+//   }
+// }
 .figure-field {
   grid-column: 2 / 12;
   grid-row: 2 / 12;
   background: #f0f8ff;
 }
-
 .title {
   font-size: 100px;
   font-weight: 300;
   color: #35495e;
   letter-spacing: 1px;
 }
-
 .subtitle {
   padding-bottom: 15px;
   font-size: 42px;
@@ -136,11 +131,9 @@ a {
   color: #526488;
   word-spacing: 5px;
 }
-
 .links {
   padding-top: 15px;
 }
-
 .button--green {
   display: inline-block;
   padding: 10px 30px;
@@ -149,12 +142,10 @@ a {
   border: 1px solid;
   border-radius: 4px;
 }
-
 .button--green:hover {
   color: #fff;
   background-color: #3b8070;
 }
-
 .button--grey {
   display: inline-block;
   padding: 10px 30px;
@@ -164,7 +155,6 @@ a {
   border: 1px solid;
   border-radius: 4px;
 }
-
 .button--grey:hover {
   color: #fff;
   background-color: #35495e;
